11c76e0382eee239ef2a6f71a362037ae9473a04
Nick DeMarchis <65045396+ndemarchis@users.noreply.github.com>
2021-08-06 14:19:47 -0400

    this is actually looking pretty good

--- src/comps/Article.js
+++ src/comps/Article.js
@@ -4,16 +4,31 @@ import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 const Article = (props) => {
 
     const {img, publication, title, url} = props.fields
-    console.log(img)
     const caption = documentToHtmlString(props.fields?.caption)
     
     return (
         <div className="article">
-            <h3 className="title"><a href={url}>{title}</a></h3>
-            <a href={url}><img src={img.fields.file.url} alt={img.fields.title} style={{maxWidth: '100%'}}/></a>
-            <h4>{publication}</h4>
-            {caption && (<p dangerouslySetInnerHTML={{__html: caption}}></p>)}
-        </div>    )
+            <h3 className="article-title">
+                <a href={url}>
+                    {title}
+                </a>
+            </h3>
+            <a href={url}>
+                <img 
+                    src={img.fields.file.url} 
+                    alt={img.fields.title} 
+                    style={{maxWidth: '100%'}}
+                    className="article-image"
+                />
+            </a>
+            <h4>
+                {publication}
+            </h4>
+            {caption && (
+                <p dangerouslySetInnerHTML={{__html: caption}}></p>
+            )}
+        </div>   
+    )
 }
 
 export default Article
--- src/comps/ContentType.js
+++ src/comps/ContentType.js
@@ -1,9 +1,28 @@
 import React, {useState} from 'react';
 import Expand from 'react-expand-animated';
+import useInterval from '../utils/useInterval';
 
 const ContentType = (title: string, data: Object, componentTemplate: JSX.Element) => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [mouseInHeading, setMouseInHeading] = useState(false);
+    const [font, setFont] = useState(['', '']);
+
+    const fontList = [
+        ['new-spirit', 700],
+        ['serif', ''], 
+        ['sans-serif', ''], 
+        ['monospace', ''], 
+        ['paralucent', 300],
+        ['interstate-mono', 700],
+        ['cortado', 400],
+        ['masqualero', 300],
+    ]
+
+    useInterval(() => {
+        // Your custom logic here
+        setFont(fontList[Math.random() * fontList.length | 0]);
+      }, mouseInHeading ? 150 : null);
 
     const components = () => {
         return Object.values(data).map(item => componentTemplate(item));
@@ -11,16 +30,34 @@ const ContentType = (title: string, data: Object, componentTemplate: JSX.Element
 
     return (
         <div>
-            <h2 
-                onClick={() => setIsOpen(!isOpen)}
-                onMouseEnter={() => {
+            <div className="headingWrapper" 
+                style={{
+
                 }}
-                onMouseLeave={() => {
-                }}
-            >{title}</h2>
-            <Expand open={isOpen}>
-                {components()}
-            </Expand>
+
+                onClick={ () => setIsOpen(!isOpen) }
+                onMouseOver={ () => { setMouseInHeading(true); } }
+                onMouseLeave={() => { setMouseInHeading(false); setFont(['', '']); }}
+            >
+                <h2 
+                    className="fancyHeading"
+                    style={{ 
+                        fontFamily: font[0], 
+                        fontWeight: font[1], 
+                        opacity: mouseInHeading ? 0.1 : 0.0, 
+                    }}
+                >{title}</h2>
+                <h2 
+                    className="notFancyHeading"
+                >{title}</h2>
+            </div>
+            <div style={{
+                padding: '10px',
+            }}>
+                <Expand open={isOpen}>
+                    {components()}
+                </Expand>
+            </div>
         </div>
     );
 };
