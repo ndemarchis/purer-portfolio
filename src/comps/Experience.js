ca60928fdc0728141befb1411a1b0d46fa29195c
Nick DeMarchis <65045396+ndemarchis@users.noreply.github.com>
2021-08-08 14:19:12 -0400

    fleshing out experiences view

--- src/comps/Experience.js
+++ src/comps/Experience.js
@@ -1,12 +1,26 @@
 import React from "react"
+import DOMPurify from "dompurify"
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 
 const Experience = (props) => {
 
-    const {dates, img, link, role, title} = props.fields
+    const {dates, img, link, role, title, caption} = props.fields
     
     return (
-        <div>
-            <h3>{title}</h3>
+        <div className = "individual-experience-wrapper" >
+            <div className = "experience-content-wrapper">
+                <a href={link}><h3>{title}</h3></a>
+                <p>{role}</p>
+                <p>{dates}</p> 
+                <p dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(
+                        documentToHtmlString(caption)
+                    )
+                }} />
+            </div>
+            <div className = "experience-image-wrapper" >
+                <img className = "experience-image" src={img.fields.file.url} alt={title} />
+            </div>
         </div>
     )
 }
